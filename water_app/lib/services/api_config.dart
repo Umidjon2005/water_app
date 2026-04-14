7bdec88750b14cb3f14003476f7b4a170b054b6b
Umidjon2005 <muxtorovumidjon879@gmail.com>
2026-04-14 10:08:55 +0500

    add requirements

--- water_app/lib/services/api_config.dart
+++ water_app/lib/services/api_config.dart
@@ -3,8 +3,8 @@ import 'package:flutter/foundation.dart';
 class ApiConfig {
   static String get baseUrl {
     if (kIsWeb) {
-      return 'http://192.168.1.7:8000';
+      return 'http://10.26.2.78';
     }
-    return 'http://192.168.1.7:8000';
+    return 'http://10.26.2.78';
   }
 }
