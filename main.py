f93b40d3a6e7b9db84752113949af83e556e6413
Umidjon2005 <muxtorovumidjon879@gmail.com>
2026-04-17 17:39:04 +0500

    temp remove db

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import SessionLocal, engine, Base
-import models
+from fastapi import FastAPI
 
 app = FastAPI()
-
-Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/")
 def home():
@@ -21,15 +9,3 @@ def home():
 @app.get("/health")
 def health():
     return {"status": "ok"}
-
-@app.post("/water")
-def create_water(name: str, status: str, db: Session = Depends(get_db)):
-    water = models.Water(name=name, status=status)
-    db.add(water)
-    db.commit()
-    db.refresh(water)
-    return water
-
-@app.get("/water")
-def get_waters(db: Session = Depends(get_db)):
-    return db.query(models.Water).all()
