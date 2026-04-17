87602d8ddc2afe31495d535e32cff8e8beecbc2c
Umidjon2005 <muxtorovumidjon879@gmail.com>
2026-04-17 17:31:19 +0500

    Fix PostgreSQL connection

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL yo‘q")
+
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
